e4f7205c62ad3539eee3faa78e42f86f567fbf39
Arthkrat <42123435+Arthkrat@users.noreply.github.com>
2019-06-27 17:14:35 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+
+$('.slider .owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    dots:false,
+    navText:[],
+    responsive:{
+        0:{
+            items:1,
+            dots:true,
+            nav:false,
+
+        },
+        600:{
+            items:1
+        },
+    }
+});
+});
